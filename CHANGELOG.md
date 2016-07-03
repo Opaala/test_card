95260537ce9e48846966efa1db63c2681b2f2019
Graeme Coupar <grambo@grambo.me.uk>
2016-07-03 15:45:15 +0100

    Release v0.1.0

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+### v0.1.0 (03/07/16)
+
+- Initial Release.
+- Supports user creation.
+- Supports shout & ping messages.
--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule TestCard.Mixfile do
 
   def project do
     [app: :test_card,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
